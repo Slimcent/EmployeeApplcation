bdf65bf575a74a8c2c3ac2ed2e14e5d5b073386e
Slimcent <33299753+Slimcent@users.noreply.github.com>
2021-11-17 09:34:18 +0100

    Main project

---
+++ Employee.Web/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Employee.Web.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Name cannot be empty")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Age cannot be empty")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Account number cannot be empty")]
+        public string AccountNumber { get; set; }
+    }
+}
